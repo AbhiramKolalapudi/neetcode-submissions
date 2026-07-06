d7b931bc6f680989ab4c8a0239669c881e372039
AbhiramKolalapudi <abhiramkolalapudi@gmail.com>
2026-07-07 00:26:31 +0530

    Add: n-queens - submission-3

---
+++ Data Structures & Algorithms/n-queens/submission-3.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        vector<vector<string>> res;
+        vector<string> curr;
+        string defaultstr;
+        vector<string> templates(n);
+        for (int i = 0; i < n; i++)
+        {
+            defaultstr.push_back('.');
+        }
+        for(int i = 0; i < n; i++)
+        {
+            string newstr = defaultstr;
+            newstr[i] = 'Q';
+            templates[i] = newstr;
+        }
+        backtrack(n, res, curr, templates);
+        return res;
+    }
+
+private:
+    void backtrack(int n, vector<vector<string>>& res, vector<string>& curr, vector<string>& templates)
+    {
+        if (curr.size() == n)
+        {
+            res.push_back(curr);
+            return;
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (safe(curr, i))
+            {
+                curr.push_back(templates[i]);
+                backtrack(n, res, curr, templates);
+                curr.pop_back();
+            }
+        }
+    }
+
+    bool safe(vector<string>& board, int pos)
+    {
+        int n = board.size();
+        bool isSafe = true;
+        if (n == 0)
+            return true;
+        
+        int size = board[0].length();
+        int leftdiag = pos - n; //we basically check the if queen exits diagonally left and right and their index will start from current position - board size (for left) and current position + board size (for right)
+        int rightdiag = pos + n;
+        for (auto& prevpos : board)
+        {
+            if (leftdiag >= 0 && prevpos[leftdiag] == 'Q')
+            {
+                return false;
+            }
+            if (rightdiag < prevpos.size() && prevpos[rightdiag] == 'Q')
+            {
+                return false;
+            }
+            if (prevpos[pos] == 'Q')
+                return false;
+            leftdiag++;
+            rightdiag--;
+        }
+        return true;
+    }
+};
