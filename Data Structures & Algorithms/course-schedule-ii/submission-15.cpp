474b6c0dec106cdee3e1804b5e341727fcf2a54a
AbhiramKolalapudi <abhiramkolalapudi@gmail.com>
2026-07-11 00:23:25 +0530

    Add: course-schedule-ii - submission-15

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-15.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        unordered_map<int,vector<int>> premap;
+        unordered_set<int> visitset;
+        unordered_set<int> cycleset;
+        vector<int> output;
+        vector<int> temp;
+        stack<int> st;
+        for (int i = 0; i < numCourses; i++)
+        {
+            premap[i] = {};
+        }
+        for (auto& prereq : prerequisites)
+        {
+            premap[prereq[0]].push_back(prereq[1]);
+        }
+        for (int i = 0; i < numCourses; i++)
+        {
+            bool res = dfs(i, premap, visitset, cycleset, st);
+            if (res == false)
+                return {};
+            else
+            {
+                while(!st.empty())
+                {
+                    temp.push_back(st.top());
+                    st.pop();
+                }
+                reverse(temp.begin(), temp.end());
+                for (int t : temp)
+                    output.push_back(t);
+                temp.clear();
+            }
+        }
+        return output;
+    }
+
+private:
+    bool dfs(int course, unordered_map<int,vector<int>>& premap, unordered_set<int>& visitset, unordered_set<int>& cycleset, stack<int>& st)
+    {
+        if (cycleset.count(course))
+            return false;
+
+        if (visitset.count(course))
+            return true;
+        
+        cycleset.insert(course);
+
+        if (premap[course].empty())
+            {
+                st.push(course);
+                cycleset.erase(course);
+                visitset.insert(course);
+                return true;
+            }
+        else
+        {
+            for (int i : premap[course])
+            {
+                if(!dfs(i, premap, visitset, cycleset, st))
+                    return false;
+            }
+            st.push(course);
+            cycleset.erase(course);
+            visitset.insert(course);
+            return true;
+        }
+    }
+};
